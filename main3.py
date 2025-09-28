815fa296551b09c8ab3dcc58bafbb8fcd28eae9d
Syed Musa Raza <syedmusaraza1993@gmail.com>
2025-09-29 00:44:15 +0530

    fast api parameter variable type

--- main3.py
+++ main3.py
@@ -3,10 +3,17 @@ from fastapi import FastAPI # import
 myapp =FastAPI()  #instance
 # we can easily start API using import, instance, decorate function
 
-@myapp.get("/")  # decorate function
-def index():
-    return {"data": "blog list"}
+@myapp.get("/blog")  # decorate function
+def index(limit = 10, published:bool = True):
 
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
+
+@myapp.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
 
 @myapp.get("/blog/{id}")
 def show(id : int):
@@ -17,3 +24,4 @@ def show(id : int):
 def comments(id):
     #fletch comments of blogs with id = id
     return {"data":{"1", "2"}}
+
