463a319ac21cce976a5e6630354ffb7c35af5c65
Syed Musa Raza <syedmusaraza1993@gmail.com>
2025-10-01 01:42:27 +0530

    fast api parameter variable type

--- user-api/main.py
+++ user-api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from router import router
+from router import router # import the APIRouter instance
 
-app = FastAPI()
+app = FastAPI() # create FASTAPI application instance
 
-app.include_router(router=router)
+app.include_router(router=router) # Mount the router to include / users endpoint
